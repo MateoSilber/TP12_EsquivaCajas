539bbfde101c8d4aae55d9bcefbc2d2c9200af8e
MateoSilber <matesilber10@gmail.com>
2026-05-11 11:56:51 -0300

    vaqueriendo

---
+++ Assets/Scripts/Controles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controles : MonoBehaviour
+{
+    public int distance = 2;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(distance, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-distance, 0, 0);
+        }
+        if (transform.position.x > 2){
+                transform.position = new Vector3(2, transform.position.y, transform.position.z);
+            }
+
+        if (transform.position.x < -2){
+            transform.position = new Vector3(-2, transform.position.y, transform.position.z);
+        }   
+    }
+}
