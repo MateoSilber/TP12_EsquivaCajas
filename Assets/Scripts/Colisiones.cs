fb264ec789890ad7b2758d6e949cbba8f430ddae
MateoSilber <matesilber10@gmail.com>
2026-05-13 10:40:43 -0300

    mov

--- Assets/Scripts/Colisiones.cs
+++ Assets/Scripts/Colisiones.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Colisiones : MonoBehaviour
 {
+    public Movimiento_Caja Movimiento_Caja;
     void OnCollisionEnter(Collision col)
     {
         if(col.gameObject.CompareTag("Player"))
         {
             Destroy(col.gameObject);
-            GetComponent<Movimiento_Caja>().speedFall = 0;
+            Movimiento_Caja.speedFall = 0;
             FindObjectOfType<UIManager>().gameOver = true;
         }
         else if(col.gameObject.CompareTag("Piso"))
@@ -17,7 +18,7 @@ public class Colisiones : MonoBehaviour
             int[] posicionesX = {-2, 0, 2};
             int xRandom = posicionesX[Random.Range(0, posicionesX.Length)];
             transform.position = new Vector3(xRandom, 7, 0);
-            GetComponent<Movimiento_Caja>().speedFall += 0.3f;
+           Movimiento_Caja.speedFall += 0.3f;
 
         }
     }
